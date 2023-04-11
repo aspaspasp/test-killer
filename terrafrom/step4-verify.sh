2d1e3fc1892976f0d256d743f878cea4183fb244
aspaspasp <34629852+aspaspasp@users.noreply.github.com>
2023-04-11 01:38:58 -0400

    step5 error

--- terrafrom/step4-verify.sh
+++ terrafrom/step4-verify.sh
@@ -1,2 +1,3 @@
 q_url =$(aws sqs get-queue-url --queue-name terraform-example-queue --region us-west-2 --query 'QueueUrl')
+if [ -n "$q_url" ]; then echo done; fi
 aws sqs get-queue-attributes --queue-url $q_url
