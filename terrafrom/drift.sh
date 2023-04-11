d9f62a849e89f5bbcd066e20ce6d6e2cc89b1e3d
aspaspasp <34629852+aspaspasp@users.noreply.github.com>
2023-04-11 02:45:02 -0400

    drift fix

--- terrafrom/drift.sh
+++ terrafrom/drift.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-q_url=$(aws sqs get-queue-url --queue-name terraform-example-queue --region us-west-2 --query 'QueueUrl')
-aws sqs untag-queue --queue-url $q_url --tag-keys "Environment"
+q_url=$(aws sqs get-queue-url --queue-name terraform-example-queue --region us-west-2 --query 'QueueUrl' --endpoint-url https://sqs.us-west-2.amazonaws.com --output text)
+aws sqs untag-queue --queue-url $q_url --tag-keys "Environment" --region us-west-2
