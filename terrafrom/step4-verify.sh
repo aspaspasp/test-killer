6a1393edd8c8e0ed512782a488ab7be141636158
aspaspasp <34629852+aspaspasp@users.noreply.github.com>
2023-04-11 03:10:32 -0400

    testesd

--- terrafrom/step4-verify.sh
+++ terrafrom/step4-verify.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 q_url=$(aws sqs get-queue-url --queue-name terraform-example-queue --region us-west-2 --query 'QueueUrl')
-if [ -z "$q_url" ]; then exit 1; fi
+if [ -z "$q_url" ]
+then 
+ exit 1
+else
+ done
+fi
 # aws sqs get-queue-attributes --queue-url $q_url
